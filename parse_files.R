7f295bb1ee72f07b02cc674e20b73c25af0161c5
Kari Silvennoinen <kari@kalifi.org>
2026-02-08 23:21:40 +0200

    fix parsing errors for malformed pages

--- parse_files.R
+++ parse_files.R
@@ -6,19 +6,37 @@ library(janitor)
 library(mirai)
 
 mirai::daemons(parallel::detectCores() - 1)
+# mirai::daemons(0)
 
-filenames <- list.files("steamstats", "\\.html$", full.names = TRUE) |>
+filenames <- list.files(
+  "steamstats",
+  "\\d{8}-stats\\.html$",
+  full.names = TRUE
+) |>
   str_subset("20190912", negate = TRUE) # 20190912 is broken
 
 # Test cases:
 # 20150218 is empty
 # 20160502 is in Swedish
+# 20200504 has just few random characters
 
 # NOTE: function has package prefixes for mirai
 read_stats <- function(fn) {
-  # print(fn)
-  stat_table <- rvest::read_html(fn, encoding = ) |>
+  print(fn)
+  doc <- rvest::read_html(fn)
+
+  # doc is empty
+  if (length(rvest::html_children(doc)) == 0) {
+    return(tibble::tibble())
+  }
+
+  stat_table <- doc |> # FIXME: set correct encoding based on date!
     rvest::html_element("div#detailStats table")
+
+  # archive.is modifies the html, dropping all classes
+  if (is.na(stat_table)) {
+    return(tibble::tibble())
+  }
 
   stat_table |>
     rvest::html_table(header = TRUE) |>
@@ -26,7 +44,7 @@ read_stats <- function(fn) {
     setNames(c("current_players", "peak_today", "game")) |>
     dplyr::filter(!dplyr::row_number() == 1) |>
     dplyr::mutate(
-      current_players = as.numeric(gsub(",", "", .current_players)),
+      current_players = as.numeric(gsub(",", "", current_players)),
       peak_today = as.numeric(gsub(",", "", peak_today)),
       # older documents are iso-8859-1 and later utf-8
       game = stringr::str_replace_all(game, "[^\\p{ASCII}]", ""),
@@ -50,5 +68,10 @@ steam_stats <- map(
 end_time <- Sys.time()
 execution_time <- end_time - start_time
 print(execution_time)
+
+# gaps in dates
+# dates <- unique(steam_stats$date)
+# expected <- seq(min(dates), max(dates), by="day")
+# expected[!expected %in% dates]
 
 saveRDS(steam_stats, "steam_stats.rds")
